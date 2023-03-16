62b69016df1cf1e776f7c8568d2a8d631c3039e6
nsburtsev <burtsev.ns@gmail.com>
2023-03-16 16:57:02 +0600

    feat: Add table view cell

---
+++ Bankey/AccountSummary/Cells/AccountSummaryCell.swift
@@ -0,0 +1,31 @@
+//
+//  AccountSummaryCell.swift
+//  Bankey
+//
+//  Created by Нюргун on 16.03.2023.
+//
+
+import Foundation
+import UIKit
+
+class AccountSummaryCell: UITableViewCell {
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setup()
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension AccountSummaryCell {
+    private func setup() {
+        
+    }
+    
+    private func layout() {
+        
+    }
+}
