964f2380144a81b639ce89878be0e20e8270de36
nsburtsev <burtsev.ns@gmail.com>
2023-04-20 18:58:26 +0600

    feat: Add logout button

---
+++ Bankey/Utils/NSNotificationName.swift
@@ -0,0 +1,12 @@
+//
+//  NSNotificationName.swift
+//  Bankey
+//
+//  Created by Нюргун on 20.04.2023.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let logout = Notification.Name("Logout")
+}
